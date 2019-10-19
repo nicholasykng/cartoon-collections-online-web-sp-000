12dcb00b32e9d7324895bec7b88ca09d9e9f5825
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 22:09:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,11 @@
 def roll_call_dwarves(array)
-  i = 0
-  while i < array.length
-    puts "#{i+1}. #{array[i]}"
-    i += 1
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
   end
 end
 
-
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
