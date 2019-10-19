2efc04d438dbcaf2fb0fb587b561db0c6e7097e9
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 22:28:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(array)
   array.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
