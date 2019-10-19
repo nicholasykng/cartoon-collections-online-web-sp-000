8f60ddea19d294cb1f998f73411633a2c7bb6b2a
Nicholas Ng <nicholasykng@gmail.com>
2019-10-19 22:03:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)
-  new = []
-  array.each_with_index(1) do |name, index|
-    new.push("#{index} #{name}")
+  i = 0
+  while i < array.length
+    puts "#{i+1}. #{array[i]}"
+    i += 1
   end
-  puts "#{new.join(" ")}"
 end
 
 
